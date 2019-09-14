5bc417e1151ee7a41b216eb98b415326873c5328
Алексей Костин <ostin654@gmail.com>
2019-09-14 22:07:57 +0300

    nRF24 w/atmega328

--- arduino/nRF24_rx/nRF24_rx.ino
+++ arduino/nRF24_rx/nRF24_rx.ino
@@ -2,6 +2,7 @@
 #include <SPI.h>
 #include <RF24.h>
 #include <LiquidCrystal_I2C.h>
+#include <GyverTimer.h>
 
 //#define LCD_ENABLE
 
@@ -9,13 +10,22 @@ RF24 radio(6, 7);
 #ifdef LCD_ENABLE
 LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 #endif
+GTimer_ms ledTimer1;
+GTimer_ms ledTimer2;
+GTimer_ms ledTimer3;
+GTimer_ms ledTimer4;
+#ifdef LCD_ENABLE
+GTimer_ms lсdTimer1;
+GTimer_ms lсdTimer2;
+GTimer_ms lсdTimer3;
+GTimer_ms lсdTimer4;
+#endif;
 
 const uint8_t pipe_out[5] = {232,205,3,145,35};
 const uint8_t pipe_in1[5] = {141,205,3,145,35};
 const uint8_t pipe_in2[5] = {142,205,3,145,35};
 const uint8_t pipe_in3[5] = {143,205,3,145,35};
 const uint8_t pipe_in4[5] = {144,205,3,145,35};
-const uint8_t pipe_in5[5] = {145,205,3,145,35};
 
 uint8_t pipeNum;
 unsigned long data = 0;
@@ -30,10 +40,6 @@ void setup()
   digitalWrite(10, LOW);
   pinMode(11, OUTPUT);
   digitalWrite(11, LOW);
-  pinMode(12, OUTPUT);
-  digitalWrite(12, LOW);
-  pinMode(13, OUTPUT);
-  digitalWrite(13, LOW);
 
   radio.begin();
   radio.powerUp();               // включение или пониженное потребление powerDown - powerUp
@@ -51,7 +57,6 @@ void setup()
   radio.openReadingPipe(2,pipe_in2);
   radio.openReadingPipe(3,pipe_in3);
   radio.openReadingPipe(4,pipe_in4);
-  radio.openReadingPipe(5,pipe_in5);
 
   //radio.printDetails();
   radio.startListening(); // приём
@@ -59,6 +64,7 @@ void setup()
   #ifdef LCD_ENABLE
   lcd.init();
   lcd.backlight();
+
   lcd.setCursor(1,0);
   lcd.print("1:----");
   lcd.setCursor(9,0);
@@ -68,100 +74,152 @@ void setup()
   lcd.setCursor(9,1);
   lcd.print("4:----");
   #endif
+
+  ledTimer1.setInterval(2000);
+  ledTimer1.setMode(MANUAL);
+  ledTimer1.reset();
+  ledTimer2.setInterval(2000);
+  ledTimer2.setMode(MANUAL);
+  ledTimer2.reset();
+  ledTimer3.setInterval(2000);
+  ledTimer3.setMode(MANUAL);
+  ledTimer3.reset();
+  ledTimer4.setInterval(2000);
+  ledTimer4.setMode(MANUAL);
+  ledTimer4.reset();
+
+  #ifdef LCD_ENABLE
+  lcdTimer1.setInterval(60000);
+  lcdTimer1.setMode(MANUAL);
+  lcdTimer1.reset();
+  lcdTimer2.setInterval(60000);
+  lcdTimer2.setMode(MANUAL);
+  lcdTimer2.reset();
+  lcdTimer3.setInterval(60000);
+  lcdTimer3.setMode(MANUAL);
+  lcdTimer3.reset();
+  lcdTimer4.setInterval(60000);
+  lcdTimer4.setMode(MANUAL);
+  lcdTimer4.reset();
+  #endif
 }
 
 void loop()  
 {
+  if (ledTimer1.isReady()) {
+    digitalWrite(8, LOW);
+    #ifdef LCD_ENABLE
+    lcd.setCursor(0,0);
+    lcd.print(" ");
+    #endif
+  }
+
+  #ifdef LCD_ENABLE
+  if (lcdTimer1.isReady()) {
+    lcd.setCursor(1,0);
+    lcd.print("1:----");
+  }
+  #endif
+
+  if (ledTimer2.isReady()) {
+    digitalWrite(9, LOW);
+    #ifdef LCD_ENABLE
+    lcd.setCursor(8,0);
+    lcd.print(" ");
+    #endif
+  }
+
+  #ifdef LCD_ENABLE
+  if (lcdTimer2.isReady()) {
+    lcd.setCursor(9,0);
+    lcd.print("2:----");
+  }
+  #endif
+
+  if (ledTimer3.isReady()) {
+    digitalWrite(10, LOW);
+    #ifdef LCD_ENABLE
+    lcd.setCursor(0,1);
+    lcd.print(" ");
+    #endif
+  }
+
+  #ifdef LCD_ENABLE
+  if (lcdTimer3.isReady()) {
+    lcd.setCursor(1,1);
+    lcd.print("3:----");
+  }
+  #endif
+
+  if (ledTimer4.isReady()) {
+    digitalWrite(11, LOW);
+    #ifdef LCD_ENABLE
+    lcd.setCursor(8,1);
+    lcd.print(" ");
+    #endif
+  }
+
+  #ifdef LCD_ENABLE
+  if (lcdTimer4.isReady()) {
+    lcd.setCursor(9,1);
+    lcd.print("4:----");
+  }
+  #endif
+
   if (radio.available(&pipeNum)) {
     radio.read(&data, sizeof(data));
 
     if (pipeNum == 1) {
       digitalWrite(8, HIGH);
+      ledTimer1.reset();
+      
       #ifdef LCD_ENABLE
+      lcdTimer1.reset();
+      
       lcd.setCursor(0,0);
       lcd.print("*");
-      lcd.setCursor(8,0);
-      lcd.print(" ");
-      lcd.setCursor(0,1);
-      lcd.print(" ");
-      lcd.setCursor(8,1);
-      lcd.print(" ");
       lcd.setCursor(3,0);
       lcd.print(data/1000);
       #endif
     }
     if (pipeNum == 2) {
       digitalWrite(9, HIGH);
+      ledTimer2.reset();
+      
       #ifdef LCD_ENABLE
-      lcd.setCursor(0,0);
-      lcd.print(" ");
+      lcdTimer2.reset();
+      
       lcd.setCursor(8,0);
       lcd.print("*");
-      lcd.setCursor(0,1);
-      lcd.print(" ");
-      lcd.setCursor(8,1);
-      lcd.print(" ");
-      lcd.setCursor(3,0);
       lcd.setCursor(11,0);
       lcd.print(data/1000);
       #endif
     }
     if (pipeNum == 3) {
       digitalWrite(10, HIGH);
+      ledTimer3.reset();
+      
       #ifdef LCD_ENABLE
-      lcd.setCursor(0,0);
-      lcd.print(" ");
-      lcd.setCursor(8,0);
-      lcd.print(" ");
+      lcdTimer3.reset();
+      
       lcd.setCursor(0,1);
       lcd.print("*");
-      lcd.setCursor(8,1);
-      lcd.print(" ");
-      lcd.setCursor(3,0);
       lcd.setCursor(3,1);
       lcd.print(data/1000);
       #endif
     }
     if (pipeNum == 4) {
       digitalWrite(11, HIGH);
+      ledTimer4.reset();
+      
       #ifdef LCD_ENABLE
-      lcd.setCursor(0,0);
-      lcd.print(" ");
-      lcd.setCursor(8,0);
-      lcd.print(" ");
-      lcd.setCursor(0,1);
-      lcd.print(" ");
+      lcdTimer4.reset();
+      
       lcd.setCursor(8,1);
       lcd.print("*");
-      lcd.setCursor(3,0);
       lcd.setCursor(11,1);
       lcd.print(data/1000);
       #endif
     }
-    if (pipeNum == 5) {
-      digitalWrite(12, HIGH);
-    }
-  } else {
-    #ifdef LCD_ENABLE
-    lcd.setCursor(0,0);
-    lcd.print(" ");
-    lcd.setCursor(8,0);
-    lcd.print(" ");
-    lcd.setCursor(0,1);
-    lcd.print(" ");
-    lcd.setCursor(8,1);
-    lcd.print(" ");
-    lcd.setCursor(3,0);
-    #endif
-
-    digitalWrite(8, LOW);
-    digitalWrite(9, LOW);
-    digitalWrite(10, LOW);
-    digitalWrite(11, LOW);
-    digitalWrite(12, LOW);
-    digitalWrite(13, HIGH);
-    delay(30);
-    digitalWrite(13, LOW);
   }
-  delay(100);
 }
--- arduino/nRF24_tx_bme/nRF24_tx_bme.ino
+++ arduino/nRF24_tx_bme/nRF24_tx_bme.ino
@@ -2,10 +2,14 @@
 #include <RF24.h>
 #include <Adafruit_BME280.h>
 #include <Adafruit_BMP280.h>
+#include <avr/sleep.h>
+#include <avr/wdt.h>
 
 #define BMP_ENABLE
+#define RADIO_SLEEP
+#define DELAY 2
 
-RF24 radio(6, 7);
+RF24 radio(9, 10);
 #ifdef BMP_ENABLE
 Adafruit_BMP280 bmx;
 #endif
@@ -16,8 +20,11 @@ const uint8_t pipe_out[5] = {141,205,3,145,35};
 bool bmxStatus = false;
 unsigned long data = 0;
 
+
 void setup()
 {
+  delay(1000);
+
   Serial.begin(9600);
   #ifdef BMP_ENABLE
   bmxStatus = bmx.begin(0x76);
@@ -27,7 +34,7 @@ void setup()
 
   if (bmxStatus) {
     radio.begin();
-    radio.powerUp();
+    radio.stopListening();
 
     radio.setDataRate(RF24_250KBPS); // скорость обмена данными RF24_250KBPS, RF24_1MBPS или RF24_2MBPS
     radio.setCRCLength(RF24_CRC_16); // длинна контрольной суммы 8-bit or 16-bit
@@ -39,6 +46,14 @@ void setup()
 
     radio.openWritingPipe(pipe_out);   // открыть канал на отправку
     radio.openReadingPipe(1,pipe_in);
+
+    #ifdef BMP_ENABLE
+    bmx.setSampling(Adafruit_BMP280::MODE_NORMAL,
+                    Adafruit_BMP280::SAMPLING_X2,
+                    Adafruit_BMP280::SAMPLING_NONE,
+                    Adafruit_BMP280::FILTER_OFF,
+                    Adafruit_BMP280::STANDBY_MS_2000);
+    #endif
   } else {
     while (1) {
       Serial.println("BME Not working");
@@ -49,16 +64,61 @@ void setup()
 
 void loop()   
 {
-  if (bmxStatus) {
+  static byte iteration = 0;
+  iteration = (iteration + 1) % DELAY;
+  if (bmxStatus && iteration==0) {
     #ifdef BMP_ENABLE
     data = (unsigned long) (bmx.readTemperature() * 1000);
     #else
     data = (unsigned long) (millis() % 10000);
     #endif
+
+    #ifdef RADIO_SLEEP
+    radio.powerUp();
+    delay(20);
+    #endif
     if (radio.write(&data, sizeof(data))) {
     } else {
       Serial.println("Not sent");
     }
+    #ifdef RADIO_SLEEP
+    radio.powerDown();
+    delay(20);
+    #endif
   }
-  delay(50);
+  goToSleep();
+}
+
+// watchdog interrupt
+ISR (WDT_vect)
+{
+  wdt_disable(); // disable watchdog
+}
+ 
+void goToSleep ()
+{
+  // disable ADC
+  ADCSRA = 0;
+  // clear various "reset" flags
+  MCUSR = 0;
+  // allow changes, disable reset
+  WDTCSR = bit (WDCE) | bit (WDE);
+  // set interrupt mode and an interval
+  WDTCSR = bit (WDIE) | bit (WDP3) | bit (WDP0); // set WDIE, and 8 seconds delay
+  wdt_reset(); // reset the watchdog
+  set_sleep_mode (SLEEP_MODE_PWR_DOWN);
+  //power_adc_disable();
+  //power_usart0_disable();
+  //power_timer0_disable();
+  //power_timer1_disable();
+  //power_timer2_disable();
+  noInterrupts (); // timed sequence follows
+  sleep_enable();
+  // turn off brown‐out enable in software
+  MCUCR = bit (BODS) | bit (BODSE);
+  MCUCR = bit (BODS);
+  interrupts (); // guarantees next instruction executed
+  sleep_cpu ();
+  // cancel sleep as a precaution
+  sleep_disable();
 }
